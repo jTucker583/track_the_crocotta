bfae692c87af716612a1f68d3c9830b8105d37b8
jake <tuckerjake11@gmail.com>
2023-05-24 10:26:28 -0600

    more cleanup

---
+++ openGL/build/CMakeFiles/SDLtest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLtest.dir/src/main.cpp.o"
+  "CMakeFiles/SDLtest.dir/src/main.cpp.o.d"
+  "SDLtest"
+  "SDLtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ openGL/build/CMakeFiles/SDLtest.exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLtest.exe.dir/src/glad.c.o"
+  "CMakeFiles/SDLtest.exe.dir/src/glad.c.o.d"
+  "CMakeFiles/SDLtest.exe.dir/src/main.cpp.o"
+  "CMakeFiles/SDLtest.exe.dir/src/main.cpp.o.d"
+  "SDLtest.exe"
+  "SDLtest.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/SDLtest.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
